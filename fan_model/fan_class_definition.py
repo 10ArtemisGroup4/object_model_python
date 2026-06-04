3d0f72e41de030e48c1e1c849bf9c60b54304c3d
unknown <triztancanlas0110@gmail.com>
2026-06-04 12:02:23 +0800

    In this commit, I am actually rushing it because I don't have much time. We have been so busy since last week and all of our free time are kind of consumed even by other subjects. But going back to the code, I declare the constructor of the class defining the speed, radius, color, and the status of the fan if it is on or not.

--- fan_model/fan_class_definition.py
+++ fan_model/fan_class_definition.py
@@ -3,8 +3,22 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
-    speed_labels = {
+    _SPEED_LABELS = {
         1: 'Slow',
         2: 'Medium',
         3: 'Fast',
     }
+
+    #constructor
+    def __init__(self, speed=None, radius=5.0, color="blue", on=False):
+        self._speed = Fan.SLOW
+        self._radius = 5.0
+        self._color = "blue"
+        self._on = False
+
+        self.speed = speed if speed is not None else Fan.SLOW
+        self.radius = radius
+        self.color = color
+        self.on = on
+
+        
