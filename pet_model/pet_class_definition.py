b4c3740981a5d93acba712a5f665359ea2683bd3
unknown <triztancanlas0110@gmail.com>
2026-06-04 15:30:11 +0800

    I also created a randomizer stuff for fun facts about animals. It is limited to some of the animals listed inside the set and that it would be nakakaumay or tiring if I added more LOL hahaha.

--- pet_model/pet_class_definition.py
+++ pet_model/pet_class_definition.py
@@ -39,3 +39,14 @@ class Pet:
     def get_age(self): return self.__age
 
     #unique feature
+def get_fun_fact(self):
+    facts = {
+        "dog": "Dogs have a sense of smell 40x stronger than humans.",
+        "cat": "Cats sleep 12-16 hours a day.",
+        "bird": "Some birds can mimic human speech.",
+        "fish": "Fish can recognize their owner's face.",
+        "rabbit": "Rabbits can jump up to 3 feet high.",
+        "hamster": "Hamster can run up to 8 miles a night on their wheel.",
+    }
+    return facts.get(self.__animal_type.lower(), "Every pet is special in its own way!"
+                     )
