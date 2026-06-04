062ac0118c445e06b523200797e1622f02e91316
unknown <triztancanlas0110@gmail.com>
2026-06-04 12:31:17 +0800

    I included the fan objects and their display properties

--- fan_model/test_fan.py
+++ fan_model/test_fan.py
@@ -16,3 +16,17 @@ def main():
     print("         Fan Class - Test Program         ")
     print("==========================================")
     print()
+
+    #fan objects
+    fan1 = Fan(speed=Fan.FAST, radius=10, color="yellow", on=True)
+    fan2 = Fan(speed=Fan.MEDIUM, radius=5, color="blue", on=False)
+
+    #display properties
+    display_fan("Fan Object 1:", fan1)
+    display_fan("Fan Object 2:", fan2)
+
+    print("===========================================")
+    print()
+
+    if __name__ == "__main__":
+        main()
