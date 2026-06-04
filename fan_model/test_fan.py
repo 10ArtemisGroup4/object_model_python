b8c3daabe3a2058b4c7891a0152d280098e75615
unknown <triztancanlas0110@gmail.com>
2026-06-04 12:24:31 +0800

    I defined here the display fan and its labels to help the user identify which one to use or to input

---
+++ fan_model/test_fan.py
@@ -0,0 +1,13 @@
+from Fan import Fan
+
+def display_fan(label, fan):
+    print(f" {label}")
+    print(f" Speed: {Fan._SPEED_LABELS[fan.speed]}")
+    print(f" Radius: {fan.radius}")
+    print(f" Color: {fan.color}")
+    print(f" On: {fan.on}")
+    print(f" Status:", end=" ")
+    fan.status()
+    print()
+
+    
